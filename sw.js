a76fa1147892b54629fe53434f14607a3ad976e6
Karen Cano <karenireneccano@gmail.com>
2018-12-01 00:14:48 +0800

    Offline experience added

--- sw.js
+++ sw.js
@@ -1,13 +1,107 @@
-importScripts('./node_modules/workbox-sw/build/workbox-sw.js');
 
-const staticAssets = [
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.5.0/workbox-sw.js');
+
+if (workbox) {
+    console.log(`Yay! Workbox is loaded 🎉`);
+
+    workbox.precaching.precacheAndRoute([]);
+
+    workbox.routing.registerRoute(
+        /(.*)\.(?:png|gif|jpg|html|css|js)/,
+        workbox.strategies.cacheFirst({
+            cacheName: 'images-cache',
+            plugins: [
+                new workbox.expiration.Plugin({
+                    maxEntries: 50,
+                    maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+                })
+            ]
+        })
+    );
+
+    const articleHandler = workbox.strategies.networkFirst({
+        cacheName: 'articles-cache',
+        plugins: [
+            new workbox.expiration.Plugin({
+                maxEntries: 50,
+            })
+        ]
+    });
+
+    workbox.routing.registerRoute(/(.*)\.(?:png|gif|jpg|html|css|js)/, args => {
+        return articleHandler.handle(args).then(response => {
+            if (!response) {
+        return caches.match('contact.html');
+    } else if (response.status === 404) {
+            return caches.match('contact.html');
+        }
+        return response;
+    });
+});
+
+    const postHandler = workbox.strategies.cacheFirst({
+        cacheName: 'posts-cache',
+        plugins: [
+            new workbox.expiration.Plugin({
+                maxEntries: 50,
+            })
+        ]
+    });
+
+    workbox.routing.registerRoute(/(.*)\.(?:png|gif|jpg|html|css|js)/, args => {
+        return postHandler.handle(args).then(response => {
+            if (response.status === 404) {
+        return caches.match('contact.html');
+    }
+    return response;
+})
+.catch(function() {
+        return caches.match('contact.html');
+    });
+});
+
+} else {
+    console.log(`Boo! Workbox didn't load 😬`);
+}
+
+const cacheName = 'cache-v1';
+const precacheResources = [
     './',
+    './index.html',
+    './images/church/church-main.jpg',
+    './images/couple/karen.png',
+    './images/couple/ivan.png',
     './favicon.ico',
-    './styles.css',
-    './app.js',
+    './css/animate.css',
+    './css/bootstrap.css',
+    './css/flexslider.css',
+    './css/icomoon.css',
+    './css/magnific-popup.css',
+    './css/owl.carousel.min.css',
+    './css/owl.theme.default.min.css',
+    './app.js'
 ];
-// const wb = new WorkboxSW();
-console.log(workbox);
 
-workbox.precaching.precache(staticAssets);
-workbox.routing.registerRoute('https://reqres.in/(.*)',workbox.strategies.networkFirst()); 
+self.addEventListener('install', event => {
+    console.log('Service worker install event!');
+    event.waitUntil(
+        caches.open(cacheName)
+            .then(cache => {
+            return cache.addAll(precacheResources);
+    }));
+});
+
+self.addEventListener('activate', event => {
+    console.log('Service worker activate event!');
+});
+
+self.addEventListener('fetch', event => {
+    console.log('Fetch intercepted for:', event.request.url);
+    event.respondWith(caches.match(event.request)
+            .then(cachedResponse => {
+            if (cachedResponse) {
+                return cachedResponse;
+            }
+            return fetch(event.request);
+    }));
+});
